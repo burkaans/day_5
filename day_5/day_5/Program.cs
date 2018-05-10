4ea5cf09551d694337f7c4c60e29280b9f92a061
burkaans <raimonds.buholts@gmail.com>
2018-05-10 18:50:36 +0300

    day_5
    pd_1_1 - 1. commits

--- day_5/day_5/Program.cs
+++ day_5/day_5/Program.cs
@@ -10,7 +10,12 @@ namespace day_5
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, Master, please tell me your name.");
+            Console.WriteLine("Type your name now.");
+            string vards = Console.ReadLine();
+            Console.WriteLine("Now type your surname, please.");
+            string uzvards = Console.ReadLine();
+            Console.WriteLine("Nice to meet you Master, " + vards + " " + uzvards + ", I'm ready to serve you now!");
             Console.ReadLine();
         }
     }
